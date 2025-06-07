67f0c929f269f61845b367a9f66cfe728eabe7db
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-06-07 07:37:11 +0000

    adding logtime to logfile

--- logtime.sh
+++ logtime.sh
@@ -5,13 +5,14 @@ log_start_hour=$(who -u | awk '{print $4}')
 
 while true
 do
+	current_date=$(date "+%Y-%m-%d");
+	current_time=$(date "+%H:%M:%S");
 
-current_date=$(date "+%Y-%m-%d");
-current_time=$(date "+%H:%M:%S");
+	echo "$log_start_date|$log_start_hour" > logs;
+	echo "$current_date|$current_time" >> logs;	
 
-echo "$log_start_date|$log_start_hour" > logs;
-echo "$current_date|$current_time" >> logs;
-./calculate;
-sleep 5;
+	logtime=$(./calculate);
+	echo "$logtime" >> logs; 
+	sleep 5;
 done
 
--- logtime_calculator.c
+++ logtime_calculator.c
@@ -53,8 +53,10 @@ void	print_logtime(int logtime_in_seconds) {
 	int hours = seconds / 3600;
 	int minutes = (seconds % 3600) / 60;
 	int secs = seconds % 60;
-
-	printf("logtime: %d hours, %d minutes, %d seconds\n", hours, minutes, secs);
+	
+	setvbuf(stdout, NULL, _IONBF, 0);
+	printf("logtime: %d hours, %d minutes, %d seconds\r", hours, minutes, secs);
+	fflush(stdout);
 }
 
 int main() {
